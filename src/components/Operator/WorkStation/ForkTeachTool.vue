e6cc7100802de4966b715d2c9629d72abea0600a
gpm <gpm@gpmcorp.com.tw>
2023-08-31 20:36:11 +0800

    ForkTeachTool.vue optimized

--- src/components/Operator/WorkStation/ForkTeachTool.vue
+++ src/components/Operator/WorkStation/ForkTeachTool.vue
@@ -1,32 +1,60 @@
 <template>
   <div class="teach-tool">
-    <el-dialog width="45%" :modal="false" draggable v-model="show" :title="title_">
-      <div class="d-flex border-bottom my-2 text-start">
-        <div>
-          <div class="height-text">{{ input_label_text }}</div>
-          <input id="teach-val-input" v-model="input_sum" @change="HandleInputChanged" />
+    <el-dialog width="50%" :modal="false" draggable v-model="show" :title="title_">
+      <div class="border-bottom my-2 text-start">
+        <div class="d-flex">
+          <div>
+            <div class="height-text">{{ input_label_text }}</div>
+            <input id="teach-val-input" v-model="input_sum" @change="HandleInputChanged" />
+          </div>
+          <div class="mx-3" v-if="!is_modify_tagNumber">
+            <div class="height-text">Pose</div>
+            <div class="goal-val">{{ goal.pose }}</div>
+          </div>
         </div>
-        <!-- <div v-if="!is_modify_tagNumber">
-          <div class="height-text">Tag</div>
-          <div class="goal-val">{{ goal.tag }}</div>
-        </div>
-        <div v-if="!is_modify_tagNumber">
-          <div class="height-text">Layer</div>
-          <div class="goal-val">{{ goal.layer }}</div>
-        </div>-->
-        <div class="mx-3" v-if="!is_modify_tagNumber">
-          <div class="height-text">Pose</div>
-          <div class="goal-val">{{ goal.pose }}</div>
+        <el-button @click="HandleForkMoveBtnClick" type="danger">移動牙叉至此位置</el-button>
+      </div>
+      <div class>
+        <SimpleKeyboardVue KeyboardVue keyboard_type="number" @onKeyPress="KeyboardInputChanged"></SimpleKeyboardVue>
+        <div class="text-start border-top py-2">
+          <div class="d-flex">
+            <h6>牙叉位置調整</h6>
+            <span class="mx-3">
+              <span>當前高度:</span>
+              {{current_position }}
+              <span>cm</span>
+            </span>
+          </div>
+          <div class="d-flex">
+            <div class="button-group d-flex p-1">
+              <el-button @click="ForkAction('home')">Home</el-button>
+              <el-button @click="ForkAction('stop')" type="danger">STOP</el-button>
+            </div>
+            <div>
+              <div class="button-group d-flex p-1">
+                <el-button @click="ForkPositionAdjust(0.1)">+1mm</el-button>
+                <el-button @click="ForkPositionAdjust(0.5)">+5mm</el-button>
+                <el-button @click="ForkPositionAdjust(-0.1)">-1mm</el-button>
+                <el-button @click="ForkPositionAdjust(-0.5)">-5mm</el-button>
+              </div>
+              <div class="button-group d-flex p-1">
+                <el-button @click="ForkPositionAdjust(1)">+1cm</el-button>
+                <el-button @click="ForkPositionAdjust(5)">+5cm</el-button>
+                <el-button @click="ForkPositionAdjust(-1)">-1cm</el-button>
+                <el-button @click="ForkPositionAdjust(-5)">-5cm</el-button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <SimpleKeyboardVue KeyboardVue keyboard_type="number" @onKeyPress="KeyboardInputChanged"></SimpleKeyboardVue>
     </el-dialog>
   </div>
 </template>
 
 <script>
 import SimpleKeyboardVue from '@/components/Tools/SimpleKeyboard.vue';
-
+import { ForkAPI } from '@/api/VMSAPI.js'
+import { AGVStatusStore } from '@/store';
 export default {
   components: {
     SimpleKeyboardVue,
@@ -72,6 +100,9 @@ export default {
     title_() {
 
       return 'Tag ' + (this.is_modify_tagNumber ? this.goal.tag : this.goal.tag + '-Layer ' + this.goal.layer);
+    },
+    current_position() {
+      return AGVStatusStore.getters.ForkHeight
     }
   },
   methods: {
@@ -80,6 +111,53 @@ export default {
       this.input_sum = this.goal.value + ''
       this.number = this.goal.value
       this.show = true
+    },
+    HandleForkMoveBtnClick() {
+      this.$swal.fire(
+        {
+          text: '請確認牙叉上方/下方是否有障礙物!',
+          title: '確定要將牙叉移動至此位置?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'OK',
+          customClass: 'my-sweetalert'
+        }).then(res => {
+          if (res.isConfirmed) {
+            setTimeout(async () => {
+              var response = await ForkAPI.Action('pose', this.number, 1)
+              if (!response.confirm) {
+                this.$swal.fire(
+                  {
+                    text: '',
+                    title: response.message,
+                    icon: 'error',
+                    showCancelButton: false,
+                    confirmButtonText: 'OK',
+                    customClass: 'my-sweetalert'
+                  })
+              }
+            }, 100);
+
+          }
+        })
+    },
+    async ForkPositionAdjust(position_delta) {
+      //confirm = result.success, message = result.message 
+      var response = await ForkAPI.Action('increase', position_delta, 1)
+      if (!response.confirm) {
+        this.$swal.fire(
+          {
+            text: '',
+            title: response.message,
+            icon: 'error',
+            showCancelButton: false,
+            confirmButtonText: 'OK',
+            customClass: 'my-sweetalert'
+          })
+      }
+    },
+    async ForkAction(action) {
+      await ForkAPI.Action(action)
     },
     HandleInputChanged() {
 
@@ -130,6 +208,11 @@ export default {
     border-radius: 4px;
     background-color: aliceblue;
     border: 2px solid grey;
+  }
+  .button-group {
+    button {
+      width: 60px;
+    }
   }
 }
 </style>
