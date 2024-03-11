11606e391ae955405136bcb7d7a0f106f5c66541
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-03-11 13:37:36 +0800

    後端連線重新復原後reload頁面

--- src/AGVDataFetchWorker.js
+++ src/AGVDataFetchWorker.js
@@ -1,7 +1,7 @@
 import { AGVStatusStore, DIOStore, RDTestDataStore, UIStore } from "./store";
 import param from "./gpm_param";
 import MapAPI from './api/MapAPI'
-
+import bus from "./event-bus";
 
 function Throttle(func, limit) {
     let inThrottle;
@@ -25,9 +25,12 @@ function generateRandomUserID(length) {
 }
 var backend_ws_host = param.backend_host.replace('http', 'ws')
 var user_id = generateRandomUserID(8);
-
+var _isDisconnceted = false;
 const throttledHandleBackendData = Throttle(function (event) {
     if (event.data != 'error' && event.data != 'closed') {
+        if (_isDisconnceted) {
+            location.reload();
+        }
         var ConnectionData = event.data.ConnectionStatesVM;
         var VMSData = event.data.VMSStatesVM;
         var DIOTableData = event.data.DIOTableVM;
@@ -37,11 +40,19 @@ const throttledHandleBackendData = Throttle(function (event) {
         DIOStore.commit('updateStatus', DIOTableData);
         RDTestDataStore.commit('SetData', testData)
 
+    } else {
+        _isDisconnceted = true;
+        bus.emit('ws_disconnect');
     }
-}, 33);
+}, 50);
 
 
 const backend_websocket_worker = new Worker('/websocket_worker.js')
+
+function DisconnectHandler() {
+
+}
+
 backend_websocket_worker.onmessage = (event) => throttledHandleBackendData(event)
 backend_websocket_worker.postMessage({ command: 'connect', ws_url: backend_ws_host + `/ws?user_id=${user_id}` });
 
