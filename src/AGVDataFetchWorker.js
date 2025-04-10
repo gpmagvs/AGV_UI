6e6291300d2c7e8076627a784ce21e173d0d7abe
jinweihuang20 <jinweihuang1020@gmail.com>
2025-04-10 09:18:33 +0800

    重構 AGVDataFetchWorker，移除不必要的代碼和功能，簡化數據處理邏輯，並優化 Hub 連接啟動流程

--- src/AGVDataFetchWorker.js
+++ src/AGVDataFetchWorker.js
@@ -3,32 +3,14 @@ import { ROS_STORE } from "./store/ros_store";
 import param from "./gpm_param";
 import MapAPI from './api/MapAPI'
 import bus from "./event-bus";
-import { ElNotification, ElLoading } from "element-plus";
-import { subscribeModuleInfoAndStore } from './api/WebRos'
 import * as signalR from "@microsoft/signalr";
 // ElLoading.service({
 //     lock: true,
 //     text: 'Loading',
 //     background: 'rgba(0, 0, 0, 0.7)',
 // })
-console.log('Fetch Worker Start');
-const channel = new BroadcastChannel('vcs_data');
-const rosDataChannel = new BroadcastChannel('ros_data');
-let _hasData = true;
-let _isLeader = false;
 var HubConnection = null;
 
-function Throttle(func, limit) {
-    let inThrottle;
-    return function () {
-        const context = this, args = arguments;
-        if (!inThrottle) {
-            func.apply(context, args);
-            inThrottle = true;
-            setTimeout(() => (inThrottle = false), limit);
-        }
-    };
-}
 function generateRandomUserID(length) {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -38,11 +20,7 @@ function generateRandomUserID(length) {
     }
     return result;
 }
-var backend_ws_host = param.backend_host.replace('http', 'ws')
 var user_id = generateRandomUserID(8);
-var _isDisconnceted = false;
-
-
 
 function StoreData(data) {
     try {
@@ -64,24 +42,6 @@ function StoreData(data) {
     }
 
 }
-
-const throttledHandleBackendData = Throttle(function (event) {
-    if (event.data != 'error' && event.data != 'closed') {
-        if (_isDisconnceted) {
-            location.reload();
-        }
-        let _data = event.data;
-        StoreData(_data);
-        var postData = {
-            type: 'data',
-            payload: _data
-        }
-        channel.postMessage(postData)
-    } else {
-        _isDisconnceted = true;
-        bus.emit('ws_disconnect');
-    }
-}, 50);
 
 function StartHubConnection() {
     HubConnection = new signalR.HubConnectionBuilder()
@@ -159,83 +119,8 @@ function StartHubConnection() {
     }
 }
 
-function BecomeLeader() {
-    if (_isLeader)
-        return;
-
-    var postData = {
-        type: 'data',
-        payload: {}
-    }
-    channel.postMessage(postData)
-    channel.postMessage({
-        type: 'heartbeat'
-    })
-    _isLeader = true;
-    // subscribeModuleInfoAndStore();
-    StartHubConnection();
-    console.log('As data brocaster');
-    setInterval(() => {
-        channel.postMessage({
-            type: 'heartbeat'
-        })
-    }, 50);
-    setInterval(() => {
-        var dataPayload = ROS_STORE.getters.Module_Information;
-        rosDataChannel.postMessage({
-            type: 'data',
-            payload: JSON.stringify(dataPayload)
-        })
-    }, 100);
-}
-
-let lastHeartbeat = Date.now();
-function checkLeaderAlive() {
-    lastHeartbeat = Date.now();
-    setInterval(() => {
-        var interval = Date.now() - lastHeartbeat;
-        if (interval > 700) {
-            _hasData = false;
-            setTimeout(() => {
-                if (!_hasData)
-                    BecomeLeader()
-            }, 700);
-
-        }
-    }, 300);
-
-}
-
 export function Start() {
-
-    setTimeout(() => {
-
-        rosDataChannel.onmessage = (event) => {
-            if (_isLeader)
-                return;
-            if (event.data.type == 'data') {
-                var moduleInfoData = JSON.parse(event.data.payload)
-                ROS_STORE.commit('update_module_info', moduleInfoData)
-            }
-        }
-
-        lastHeartbeat = Date.now();
-        channel.onmessage = (event) => {
-            if (_isLeader)
-                return;
-            if (event.data.type == 'data') {
-                lastHeartbeat = Date.now();
-                _hasData = true;
-                var _data = event.data.payload;
-                StoreData(_data);
-            } else if (event.data.type == 'heartbeat') {
-                _hasData = true;
-                lastHeartbeat = Date.now();
-            }
-        }
-        checkLeaderAlive();
-
-    }, 800);
+    StartHubConnection();
     MapAPI.GetMapFromServer()
 }
 
