152314d12974c9b46f28e62a446aa0dec3edc8c0
jinweihuang20 <jinweihuang1020@gmail.com>
2025-04-15 13:11:56 +0800

    feat: refactor CheckParamSettingButton to use dynamic form items and improve switch handling

--- src/components/MainContent/CheckParamSettingButton.vue
+++ src/components/MainContent/CheckParamSettingButton.vue
@@ -15,6 +15,8 @@
           :model="form"
           label-position="left"
           label-width="90%"
+          :rules="rules"
+          :ref="formSettings"
         >
           <el-divider class="mb-3">
             <span class="text-primary">
@@ -22,74 +24,39 @@
               <b>[需開啟]</b> 項目
             </span>
           </el-divider>
+          <el-form-item
+            v-for="item in needOpenItems"
+            :key="item.key"
+            :label="item.label"
+            :prop="item.key"
+            v-bind:class="{'highlighted-form-label': item.value==false }"
+          >
+            <el-switch
+              :disabled="!IsGodLogin"
+              @change="handleSwitchChanged(item.value,item.key)"
+              size="small"
+              v-model="item.value"
+            />
+          </el-form-item>
 
-          <el-form-item label="取貨須拍照">
-            <el-switch
-              @change="handleSwitchChanged"
-              size="small"
-              v-model="formSettings.CST_READER_TRIGGER"
-            />
-          </el-form-item>
-          <el-form-item label="貨物狀態偵測(有貨移動中、取放貨過程偵測貨物是否偏移或不該存在)">
-            <el-switch
-              @change="handleSwitchChanged"
-              size="small"
-              v-model="formSettings.CargoBiasDetectionWhenNormalMoving"
-            />
-          </el-form-item>
-          <el-form-item label="自設備/WIP退出時須詢問派車系統">
-            <el-switch
-              @change="handleSwitchChanged"
-              size="small"
-              v-model="formSettings.LDULDParams.LeaveWorkStationNeedSendRequestToAGVS"
-            />
-          </el-form-item>
-          <el-form-item label="當[取貨]時，檢查貨物是否已放置完全/當[放貨]時，檢查貨物是否已移除完成">
-            <el-switch
-              @change="handleSwitchChanged"
-              size="small"
-              v-model="formSettings.CST_EXIST_DETECTION.After_EQ_Busy_Off"
-            />
-          </el-form-item>
-          <el-form-item label="準備[取貨]前，檢查車上無貨物; 準備[放貨]前，檢查車上貨物放置完全">
-            <el-switch
-              @change="handleSwitchChanged"
-              size="small"
-              v-model="formSettings.CST_EXIST_DETECTION.Before_In"
-            />
-          </el-form-item>
-          <el-form-item label="[放貨]前使用雷射偵測Port內是否有障礙物/貨物">
-            <el-switch
-              @change="handleSwitchChanged"
-              size="small"
-              v-model="formSettings.LDULDParams.LsrObstacleDetectionEnable"
-            />
-          </el-form-item>
-          <el-form-item label="[放貨]時使用二重檢知感測器偵測Port內是否有障礙物/貨物">
-            <el-switch
-              @change="handleSwitchChanged"
-              size="small"
-              v-model="formSettings.LOAD_OBS_DETECTION.Enable_Load"
-            />
-          </el-form-item>
-          <el-form-item label="[取貨]時使用二重檢知感測器偵測Port內是否有障礙物/貨物">
-            <el-switch
-              @change="handleSwitchChanged"
-              size="small"
-              v-model="formSettings.LOAD_OBS_DETECTION.Enable_UnLoad"
-            />
-          </el-form-item>
           <el-divider class="mb-3">
             <span class="text-danger">
               無特殊需求
               <b>[需關閉]</b> 項目
             </span>
           </el-divider>
-          <el-form-item label="空取空放">
+          <el-form-item
+            v-for="item in needCloseItems"
+            :key="item.key"
+            :label="item.label"
+            :prop="item.key"
+            v-bind:class="{'highlighted-form-label': item.value==true }"
+          >
             <el-switch
-              @change="handleSwitchChanged"
+              :disabled="!IsGodLogin"
+              @change="handleSwitchChanged(item.value,item.key)"
               size="small"
-              v-model="formSettings.LDULD_Task_No_Entry"
+              v-model="item.value"
             />
           </el-form-item>
         </el-form>
@@ -107,12 +74,80 @@ import { ref, computed } from 'vue'
 import { SystemSettingsStore, UserStore } from '@/store'
 import { SystemAPI } from '@/api/VMSAPI.js'
 import { ElNotification } from 'element-plus'
+import _ from 'lodash';
 
 const loading = ref(true)
 const dialogVisible = ref(false)
 const form = ref({})
 const formSettings = ref({})
 const IsGodLogin = computed(() => UserStore.state.UserState.Role >= 2)
+
+
+const checkItems = ref([
+  {
+    key: 'CST_READER_TRIGGER',
+    label: '取貨須拍照',
+    value: true,
+    needOpen: true,
+
+  },
+  {
+    key: 'CargoBiasDetectionWhenNormalMoving',
+    label: '貨物狀態偵測(有貨移動中、取放貨過程偵測貨物是否偏移或不該存在)',
+    value: true,
+    needOpen: true
+  },
+  {
+    key: 'LDULDParams.LeaveWorkStationNeedSendRequestToAGVS',
+    label: '自設備/WIP退出時須詢問派車系統',
+    value: true,
+    needOpen: true
+  },
+  {
+    key: 'CST_EXIST_DETECTION.After_EQ_Busy_Off',
+    label: '當[取貨]時，檢查貨物是否已放置完全/當[放貨]時，檢查貨物是否已移除完成',
+    value: true,
+    needOpen: true
+  },
+  {
+    key: 'CST_EXIST_DETECTION.Before_In',
+    label: '準備[取貨]前，檢查車上無貨物; 準備[放貨]前，檢查車上貨物放置完全',
+    value: true,
+    needOpen: true
+  },
+  {
+    key: 'LDULDParams.LsrObstacleDetectionEnable',
+    label: '[放貨]前使用雷射偵測Port內是否有障礙物/貨物',
+    value: true,
+    needOpen: true
+  },
+  {
+    key: 'LOAD_OBS_DETECTION.Enable_Load',
+    label: '[放貨]時使用二重檢知感測器偵測Port內是否有障礙物/貨物',
+    value: true,
+    needOpen: true
+  },
+  {
+    key: 'LOAD_OBS_DETECTION.Enable_UnLoad',
+    label: '[取貨]時使用二重檢知感測器偵測Port內是否有障礙物/貨物',
+    value: true,
+    needOpen: true
+  },
+  {
+    key: 'LDULD_Task_No_Entry',
+    label: '空取空放',
+    value: false,
+    needOpen: false
+  },
+])
+
+const needOpenItems = computed(() => {
+  return checkItems.value.filter(item => item.needOpen === true)
+})
+const needCloseItems = computed(() => {
+  return checkItems.value.filter(item => item.needOpen === false)
+})
+
 const checkParamSetting = async () => {
   loading.value = true
   dialogVisible.value = true
@@ -122,13 +157,18 @@ const checkParamSetting = async () => {
     await SystemSettingsStore.dispatch('downloadSettings');
   }
   formSettings.value = JSON.parse(JSON.stringify(SystemSettingsStore.state.Settings));
+
+  checkItems.value.forEach(item => {
+    let val = _.get(formSettings.value, item.key);
+    item.value = val
+  })
+
   loading.value = false
 }
 
 const handleSwitchChanged = async (value, key) => {
+  _.set(formSettings.value, key, value)
   const result = await SystemAPI.SaveSettings(formSettings.value)
-  //{"confirm":true,"errorMsg":""}
-
   if (result && result.confirm) {
     ElNotification({
       title: '參數確認',
@@ -171,5 +211,12 @@ const handleSwitchChanged = async (value, key) => {
 .content-container {
   height: 70vh;
   overflow-y: auto;
+
+  .highlighted-form-label {
+    :deep(label) {
+      font-weight: bold;
+      color: red;
+    }
+  }
 }
 </style>
